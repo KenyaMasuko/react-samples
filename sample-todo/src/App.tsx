a8a83d8dd4c0c7176e9df03012fc70a914fbcd47
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-02-02 18:19:50 +0900

    feat: sample-todoを実装

--- sample-todo/src/App.tsx
+++ sample-todo/src/App.tsx
@@ -1,34 +1,125 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { FormEvent, ReactElement, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Todo = {
+	id: number;
+	title: string;
+	isDone: boolean;
+};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const DEFAULT_TODOS: Todo[] = [
+	{
+		id: 1,
+		title: "買い物に行く",
+		isDone: false,
+	},
+	{
+		id: 2,
+		title: "部屋を掃除する",
+		isDone: false,
+	},
+	{
+		id: 3,
+		title: "夕飯を作る",
+		isDone: false,
+	},
+];
+
+function App(): ReactElement {
+	const [todos, setTodos] = useState<Todo[]>(DEFAULT_TODOS);
+	const [title, setTitle] = useState("");
+	const [editId, setEditId] = useState(0);
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		if (editId !== 0) {
+			setTodos((prevTodos) => {
+				return prevTodos.map((todo) => {
+					if (todo.id === editId) {
+						return { ...todo, title };
+					}
+
+					return todo;
+				});
+			});
+			setEditId(0);
+		} else {
+			setTodos((prev) => {
+				const newTodo = {
+					id: prev.length + 1,
+					title,
+					isDone: false,
+				};
+
+				return [...prev, newTodo];
+			});
+		}
+		setTitle("");
+	};
+
+	const handleReset = () => {
+		setEditId(0);
+		setTitle("");
+	};
+
+	const handleUpdate = (todo: Todo) => {
+		setEditId(todo.id);
+		setTitle(todo.title);
+	};
+
+	const handleRemove = (id: Todo["id"]) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter((todo) => todo.id !== id);
+		});
+	};
+
+	return (
+		<div className="App">
+			<h1>TODOS</h1>
+			<form
+				onSubmit={handleSubmit}
+				onReset={handleReset}
+				style={{ display: "flex", alignItems: "center", gap: 10 }}>
+				<input
+					type="text"
+					value={title}
+					onChange={(prevState) => setTitle(prevState.currentTarget.value)}
+					placeholder="todoを記入..."
+					style={{ padding: 10 }}
+				/>
+				<button type="submit">{editId !== 0 ? "更新" : "作成"}</button>
+				<button type="reset">リセット</button>
+			</form>
+			<table style={{ marginTop: 30 }}>
+				<thead>
+					<tr>
+						<th style={{ textAlignLast: "justify", width: 150 }}>TITLE</th>
+						<th style={{ width: 80 }}>DONE</th>
+						<th style={{ width: 80 }}>ACTION</th>
+					</tr>
+				</thead>
+				<tbody>
+					{todos?.map((todo) => (
+						<tr key={todo.id}>
+							<td style={{ textAlignLast: "justify" }}>{todo.title}</td>
+							<td>
+								<input
+									type="checkbox"
+									checked={todo.isDone}
+									onChange={() => handleRemove(todo.id)}
+								/>
+							</td>
+							<td>
+								<button onClick={() => handleUpdate(todo)}>更新</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			<ul></ul>
+		</div>
+	);
 }
 
-export default App
+export default App;
