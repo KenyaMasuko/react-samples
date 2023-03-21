e71a3bd11881ee90dfa44a05611ee40a9628ba5d
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-03-21 10:15:53 +0900

    feat: blogを実装

--- next-blog/next.config.js
+++ next-blog/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["images.unsplash.com", "flowbite.com"],
+		domains: [
+			"images.unsplash.com",
+			"flowbite.com",
+			"images.microcms-assets.io",
+		],
 	},
 };
 
---
+++ next-blog/src/pages/blog/[blogId].tsx
@@ -0,0 +1,51 @@
+import { Layout } from "@/components/Layout";
+import { client } from "@/libs/client";
+import Head from "next/head";
+
+export const getStaticPaths = async () => {
+	const data = await client.get({ endpoint: "blogs" });
+
+	const paths = data.contents.map((content: any) => `/blog/${content.id}`);
+	return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context: any) => {
+	const id = context.params.id;
+	const data = await client.get({ endpoint: "blogs", contentId: id });
+
+	return {
+		props: {
+			blog: data.contents[0],
+		},
+	};
+};
+
+const BlogDetail = ({ blog }: { blog: any }) => {
+	console.log(blog);
+
+	return (
+		<>
+			<Head>
+				<title>{blog.title}</title>
+				<meta
+					name="description"
+					content="Next.jsで作るHeadless CMSブログ - 詳細ページ"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Layout>
+				<div className="bg-white py-6 sm:py-8 lg:py-12">
+					<div className="max-w-screen-md px-4 md:px-8 mx-auto">
+						<div
+							dangerouslySetInnerHTML={{
+								__html: `${blog.content}`,
+							}}></div>
+					</div>
+				</div>
+			</Layout>
+		</>
+	);
+};
+
+export default BlogDetail;
